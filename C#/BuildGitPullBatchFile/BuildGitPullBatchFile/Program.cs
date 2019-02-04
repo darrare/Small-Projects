2f7882cb1a06fbc75b8faa01e3b92d39cfc8ec6b
darrare <ryandarras@gmail.com>
2019-02-03 22:52:36 -0700

    no message

---
+++ C#/BuildGitPullBatchFile/BuildGitPullBatchFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BuildGitPullBatchFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] directories = Directory.GetDirectories(@"D:\Repositories\3020 Class Repositories");
+            using (StreamWriter sr = new StreamWriter(@"D:\Repositories\3020 Class Repositories\file.txt"))
+            {
+                for(int i = 0; i < directories.Length; i++)
+                {
+                    sr.WriteLine(@"cd " + directories[i] + " 2>> ..\\CDError.txt");
+                    //sr.WriteLine("git clean -f");
+                    sr.WriteLine("git fetch origin 2>> ..\\FetchError.txt");
+                    sr.WriteLine("git reset --hard origin/master 2>> ..\\ResetError.txt");
+                }
+            }
+        }
+    }
+}
