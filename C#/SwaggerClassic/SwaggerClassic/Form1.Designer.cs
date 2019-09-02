eaa00ce8d28e245b0caa1996836b1390012b79e9
darrare <ryandarras@gmail.com>
2019-09-01 21:19:07 -0600

    Fully afk now

--- C#/SwaggerClassic/SwaggerClassic/Form1.Designer.cs
+++ C#/SwaggerClassic/SwaggerClassic/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SwaggerClassic
 {
-    partial class Form1
+    partial class SwaggerClassic
     {
         /// <summary>
         /// Required designer variable.
@@ -28,34 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Button_Toggle = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.Button_RandomMovement = new System.Windows.Forms.Button();
+            this.Button_QueuePasser = new System.Windows.Forms.Button();
+            this.PictureBox_PixelBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Label_MatchPercentage = new System.Windows.Forms.Label();
+            this.TextBox_Log = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PictureBox_PixelBoxCurrent = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_PixelBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_PixelBoxCurrent)).BeginInit();
             this.SuspendLayout();
             // 
-            // Button_Toggle
+            // Button_RandomMovement
             // 
-            this.Button_Toggle.Location = new System.Drawing.Point(12, 12);
-            this.Button_Toggle.Name = "Button_Toggle";
-            this.Button_Toggle.Size = new System.Drawing.Size(148, 74);
-            this.Button_Toggle.TabIndex = 0;
-            this.Button_Toggle.Text = "Start";
-            this.Button_Toggle.UseVisualStyleBackColor = true;
-            this.Button_Toggle.Click += new System.EventHandler(this.Button_Toggle_Click);
+            this.Button_RandomMovement.Location = new System.Drawing.Point(166, 276);
+            this.Button_RandomMovement.Name = "Button_RandomMovement";
+            this.Button_RandomMovement.Size = new System.Drawing.Size(148, 36);
+            this.Button_RandomMovement.TabIndex = 0;
+            this.Button_RandomMovement.Text = "Start Random Movement";
+            this.Button_RandomMovement.UseVisualStyleBackColor = true;
+            this.Button_RandomMovement.Click += new System.EventHandler(this.Button_RandomMovement_Click);
             // 
-            // Form1
+            // Button_QueuePasser
+            // 
+            this.Button_QueuePasser.Location = new System.Drawing.Point(12, 276);
+            this.Button_QueuePasser.Name = "Button_QueuePasser";
+            this.Button_QueuePasser.Size = new System.Drawing.Size(148, 36);
+            this.Button_QueuePasser.TabIndex = 1;
+            this.Button_QueuePasser.Text = "Start Queue Passer";
+            this.Button_QueuePasser.UseVisualStyleBackColor = true;
+            this.Button_QueuePasser.Click += new System.EventHandler(this.Button_QueuePasser_Click);
+            // 
+            // PictureBox_PixelBoxOriginal
+            // 
+            this.PictureBox_PixelBoxOriginal.Location = new System.Drawing.Point(13, 141);
+            this.PictureBox_PixelBoxOriginal.Name = "PictureBox_PixelBoxOriginal";
+            this.PictureBox_PixelBoxOriginal.Size = new System.Drawing.Size(302, 55);
+            this.PictureBox_PixelBoxOriginal.TabIndex = 2;
+            this.PictureBox_PixelBoxOriginal.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Label_MatchPercentage
+            // 
+            this.Label_MatchPercentage.AutoSize = true;
+            this.Label_MatchPercentage.Location = new System.Drawing.Point(163, 125);
+            this.Label_MatchPercentage.Name = "Label_MatchPercentage";
+            this.Label_MatchPercentage.Size = new System.Drawing.Size(97, 13);
+            this.Label_MatchPercentage.TabIndex = 5;
+            this.Label_MatchPercentage.Text = "Match Percent: 0%";
+            // 
+            // TextBox_Log
+            // 
+            this.TextBox_Log.Location = new System.Drawing.Point(12, 29);
+            this.TextBox_Log.Multiline = true;
+            this.TextBox_Log.Name = "TextBox_Log";
+            this.TextBox_Log.Size = new System.Drawing.Size(302, 93);
+            this.TextBox_Log.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Information Log";
+            // 
+            // PictureBox_PixelBoxCurrent
+            // 
+            this.PictureBox_PixelBoxCurrent.Location = new System.Drawing.Point(12, 215);
+            this.PictureBox_PixelBoxCurrent.Name = "PictureBox_PixelBoxCurrent";
+            this.PictureBox_PixelBoxCurrent.Size = new System.Drawing.Size(302, 55);
+            this.PictureBox_PixelBoxCurrent.TabIndex = 8;
+            this.PictureBox_PixelBoxCurrent.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Current";
+            // 
+            // SwaggerClassic
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(176, 103);
-            this.Controls.Add(this.Button_Toggle);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(327, 324);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PictureBox_PixelBoxCurrent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBox_Log);
+            this.Controls.Add(this.Label_MatchPercentage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PictureBox_PixelBoxOriginal);
+            this.Controls.Add(this.Button_QueuePasser);
+            this.Controls.Add(this.Button_RandomMovement);
+            this.Name = "SwaggerClassic";
+            this.Text = "Swagger Classic";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_PixelBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_PixelBoxCurrent)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button Button_Toggle;
+        private System.Windows.Forms.Button Button_RandomMovement;
+        private System.Windows.Forms.Button Button_QueuePasser;
+        private System.Windows.Forms.PictureBox PictureBox_PixelBoxOriginal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label Label_MatchPercentage;
+        private System.Windows.Forms.TextBox TextBox_Log;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox PictureBox_PixelBoxCurrent;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- C#/SwaggerClassic/SwaggerClassic/Form1.cs
+++ C#/SwaggerClassic/SwaggerClassic/Form1.cs
@@ -11,10 +11,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using System.Timers;
+using Timer = System.Timers.Timer;
+
+using Gma.System.MouseKeyHook;
+using System.Drawing.Imaging;
 
 namespace SwaggerClassic
 {
-    public partial class Form1 : Form
+    public partial class SwaggerClassic : Form
     {
         [DllImport("User32.dll")]
         static extern int SetForegroundWindow(IntPtr point);
@@ -28,6 +32,11 @@ namespace SwaggerClassic
         [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
         private static extern IntPtr GetForegroundWindow();
 
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
         const int KEY_DOWN_EVENT = 0x0001; //Key down flag
         const int KEY_UP_EVENT = 0x0002; //Key up flag
 
@@ -35,6 +44,8 @@ namespace SwaggerClassic
         const int A_KEY = 0x41;
         const int S_KEY = 0x53;
         const int D_KEY = 0x44;
+
+        const int ENTER_KEY = 0x0D;
 
         int[] keys = new int[] { 0x57, 0x41, 0x53, 0x44 };
 
@@ -58,71 +69,253 @@ namespace SwaggerClassic
 
         #endregion
 
-        public Form1()
+        private IKeyboardMouseEvents m_Events;
+        Point imageCaptureLocation;
+        Point mousePos;
+
+        Timer drawRectangleTimer;
+
+        public SwaggerClassic()
         {
             ct = cts.Token;
             rand = new Random();
+            m_Events = Hook.GlobalEvents();
+            drawRectangleTimer = new Timer();
+            drawRectangleTimer.Interval = 100;
+            drawRectangleTimer.Enabled = true;
+
             InitializeComponent();
         }
 
-        private void Button_Toggle_Click(object sender, EventArgs e)
+        private void ConsoleLog(string s)
+        {
+            TextBox_Log.Invoke(new MethodInvoker(delegate () 
+            {
+                TextBox_Log.AppendText(s + Environment.NewLine);
+                TextBox_Log.ScrollToCaret();
+            }));
+        }
+
+        Pen myPen = new Pen(Color.Red, 3);
+        private void HookManager_MouseMove(object sender, MouseEventArgs e)
+        {
+            mousePos.X = e.X;
+            mousePos.Y = e.Y;
+        }
+
+        private void DrawRectangle_Tick(object sender, EventArgs e)
+        {
+            Point topLeft = new Point(mousePos.X - PictureBox_PixelBoxOriginal.Width / 2, mousePos.Y - PictureBox_PixelBoxOriginal.Height / 2);
+            g.DrawRectangle(myPen, new Rectangle(topLeft.X, topLeft.Y, PictureBox_PixelBoxOriginal.Width, PictureBox_PixelBoxOriginal.Height));
+        }
+
+        public void OnMouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                ConsoleLog("Section selected, displaying on picture boxes.");
+                ConsoleLog("You can go afk now.");
+                mousePos.X = e.X;
+                mousePos.Y = e.Y;
+                imageCaptureLocation = new Point(mousePos.X, mousePos.Y);
+
+                //switch to different state;
+                m_Events.MouseClick -= OnMouseClick;
+                m_Events.MouseMove -= HookManager_MouseMove;
+                drawRectangleTimer.Elapsed -= DrawRectangle_Tick;
+                ReleaseDC(IntPtr.Zero, desktopPtr);
+
+                Bitmap shotTook = new Bitmap(PictureBox_PixelBoxOriginal.Width, PictureBox_PixelBoxOriginal.Height, PixelFormat.Format16bppRgb565);
+
+                using (Graphics draw = Graphics.FromImage(shotTook))
+                {
+                    draw.CopyFromScreen(new Point(mousePos.X - PictureBox_PixelBoxOriginal.Width / 2, mousePos.Y - PictureBox_PixelBoxOriginal.Height / 2), new Point(0, 0), PictureBox_PixelBoxOriginal.Size);
+                }
+
+                PictureBox_PixelBoxOriginal.Image = (Image)shotTook;
+
+                cts = new CancellationTokenSource();
+                ct = cts.Token;
+                timingTask = Task.Factory.StartNew(TrackDifferencesBetweenImages, ct);
+            }
+        }
+
+        public void TrackDifferencesBetweenImages()
+        {
+            while(!ct.IsCancellationRequested)
+            {
+                CheckQueueDisplay_Tick();
+                Thread.Sleep(1500);
+            }
+        }
+
+        public void CheckQueueDisplay_Tick()
+        {
+            Bitmap shotTook = new Bitmap(PictureBox_PixelBoxCurrent.Width, PictureBox_PixelBoxCurrent.Height, PixelFormat.Format16bppRgb565);
+
+            using (Graphics draw = Graphics.FromImage(shotTook))
+            {
+                draw.CopyFromScreen(new Point(mousePos.X - PictureBox_PixelBoxCurrent.Width / 2, mousePos.Y - PictureBox_PixelBoxCurrent.Height / 2), new Point(0, 0), PictureBox_PixelBoxCurrent.Size);
+            }
+            
+            PictureBox_PixelBoxCurrent.Image = (Image)shotTook;
+            Thread.Sleep(500);
+            float similarity = CalculateSimilarityBetweenBitmaps(shotTook, (Bitmap)PictureBox_PixelBoxOriginal.Image);
+
+            Label_MatchPercentage.Invoke(new MethodInvoker(delegate () { Label_MatchPercentage.Text = "Match Percentage = " + similarity + "%"; }));
+            
+            if (similarity <= 33.33f)
+            {
+                //Start other buttons effects
+                ConsoleLog("Less than 1/3 similar, pausing for 10 seconds before picking character.");
+                Thread.Sleep(10000);
+                ConsoleLog("Pressing the enter key to log into character");
+                keybd_event(ENTER_KEY, 0, KEY_DOWN_EVENT, 0);
+                Thread.Sleep(100);
+                keybd_event(ENTER_KEY, 0, KEY_UP_EVENT, 0);
+                Thread.Sleep(1000);
+                ConsoleLog("Starting random movement function.");
+                RandomMovement();
+            }
+        }
+
+        private float CalculateSimilarityBetweenBitmaps(Bitmap a, Bitmap b)
+        {
+            if (a.Width != b.Width || a.Height != b.Height)
+                return 0;
+
+            float sum = 0;
+            int count = 0;
+            for (int x = 0; x < a.Width; x += 2)
+            {
+                for (int y = 0; y < a.Height; y += 2)
+                {
+                    sum += GetColorDifference(a.GetPixel(x, y), b.GetPixel(x, y));
+                    count++;
+                }
+            }
+            return 100 - ((sum / count) * 100);
+        }
+
+        /// <summary>
+        /// Only care about differences larger than a total of 3 pixel colors
+        /// </summary>
+        private float GetColorDifference(Color a, Color b)
+        {
+            int difference = Math.Abs(a.R - b.R) + Math.Abs(a.G - b.G) + Math.Abs(a.B - b.B);
+            return difference > 25 ? 1 : 0;
+        }
+
+        private void RandomMovement()
+        {
+            while (!ct.IsCancellationRequested)
+            {
+                byte randomKey = RandomKeySelector();
+                keybd_event(randomKey, 0, KEY_DOWN_EVENT, 0);
+                int time = rand.Next(minTimeToHoldButton, maxTimeToHoldButton) / 10;
+                ConsoleLog("Pressing key for " + time * 10 + "ms");
+                for (int i = 0; i < time; i++)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                    Thread.Sleep(10);
+                }
+                keybd_event(randomKey, 0, KEY_UP_EVENT, 0);
+                if (ct.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                time = rand.Next(minTimeDelay, maxTimeDelay) / 10;
+                ConsoleLog("Waiting for " + time * 10 + "ms");
+                //This for loop handles the delay between key presses
+                for (int i = 0; i < rand.Next(minTimeDelay, maxTimeDelay) / 10; i++)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                    Thread.Sleep(10);
+                }
+                if (ct.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Button_RandomMovement_Click(object sender, EventArgs e)
         {
             isActive = !isActive;
             if (isActive)
             {
+                ConsoleLog("Clicked Random Movement button");
+                ConsoleLog("You can go afk now.");
                 wow = Process.GetProcessesByName("Wow").FirstOrDefault(); //Wow
                 IntPtr h = wow.MainWindowHandle;
                 SetForegroundWindow(h);
                 cts = new CancellationTokenSource();
                 ct = cts.Token;
-                timingTask = Task.Factory.StartNew(() =>
-                {
-                    while(!ct.IsCancellationRequested)
-                    {
-                        byte randomKey = RandomKeySelector();
-                        keybd_event(randomKey, 0, KEY_DOWN_EVENT, 0);
-                        for (int i = 0; i < rand.Next(minTimeToHoldButton, maxTimeToHoldButton) / 10; i++)
-                        {
-                            if (ct.IsCancellationRequested)
-                            {
-                                break;
-                            }
-                            Thread.Sleep(10);
-                        }
-                        keybd_event(randomKey, 0, KEY_UP_EVENT, 0);
-                        if (ct.IsCancellationRequested)
-                        {
-                            break;
-                        }
-
-                        //This for loop handles the delay between key presses
-                        for (int i = 0; i < rand.Next(minTimeDelay, maxTimeDelay) / 10; i++)
-                        {
-                            if (ct.IsCancellationRequested)
-                            {
-                                break;
-                            }
-                            Thread.Sleep(10);
-                        }
-                        if (ct.IsCancellationRequested)
-                        {
-                            break;
-                        }
-                    }
-                }, cts.Token);
-                Button_Toggle.Text = "STOP";
+                timingTask = Task.Factory.StartNew(RandomMovement, cts.Token);
+                Button_RandomMovement.Text = "STOP";
+                Button_QueuePasser.Text = "STOP";
             }
             else
             {
                 cts.Cancel();
                 cts.Dispose();
-                Button_Toggle.Text = "Start";
+                Button_RandomMovement.Text = "Start Random Movement";
+                Button_QueuePasser.Text = "Start Queue Passer";
+                ConsoleLog("--- Default state ---");
             }
         }
 
         private byte RandomKeySelector()
         {
             return (byte)keys[rand.Next(0, keys.Length)];
+        }
+
+        IntPtr desktopPtr;
+        Graphics g;
+        IntPtr h;
+        private void Button_QueuePasser_Click(object sender, EventArgs e)
+        {
+            isActive = !isActive;
+            if (isActive)
+            {
+                ConsoleLog("Clicked Queue Passer button");
+                ConsoleLog("Right click the area you want to consider.");
+                desktopPtr = GetDC(IntPtr.Zero);
+                g = Graphics.FromHdc(desktopPtr);
+
+                
+
+                m_Events.MouseClick += OnMouseClick;
+                m_Events.MouseMove += HookManager_MouseMove;
+                drawRectangleTimer.Elapsed += DrawRectangle_Tick;
+
+                //wow = Process.GetProcessesByName("Wow").FirstOrDefault(); //Wow
+                //h = wow.MainWindowHandle;
+                //SetForegroundWindow(h);
+
+
+                Button_RandomMovement.Text = "STOP";
+                Button_QueuePasser.Text = "STOP";
+            }
+            else
+            {
+                cts.Cancel();
+                cts.Dispose();
+                m_Events.MouseClick -= OnMouseClick;
+                m_Events.MouseMove -= HookManager_MouseMove;
+                drawRectangleTimer.Elapsed -= DrawRectangle_Tick;
+                ReleaseDC(IntPtr.Zero, desktopPtr);
+                Button_RandomMovement.Text = "Start Random Movement";
+                Button_QueuePasser.Text = "Start Queue Passer";
+                ConsoleLog("--- Default state ---");
+            }
         }
     }
 }
